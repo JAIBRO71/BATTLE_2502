07384486adf77d7d802508dd3b54f16011a58119
JAIBRO71 <bjaikhumar@gmail.com>
2026-03-30 22:45:20 +0530

    Fixed frontend backend route

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ async function resolveConflict() {
     `;
 
     try {
-        const response = await fetch("http://127.0.0.1:5000/resolve", {
+        const response = await fetch("/resolve", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
